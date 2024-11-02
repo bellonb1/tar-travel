39606a7976fb3fdd69cb5fbe927d01423360c865
Justin <thisisjustinmao@gmail.com>
2024-11-02 14:06:35 -0400

    Add beautiful soup scraping

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://mycarolina.unc.edu/portal/study_abroad_preapproved_courses'
+url = 'https://heelsabroad.unc.edu/_portal/tds-program-brochure?programid=1253'
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text, 'html')
 
 soup.find('program_id')
+print(soup)
+
+valid_ids = []
 
